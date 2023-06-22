eb3b51ccaa65ffefcf1600597e89b8f264dc448a
Ariel Rosero <ariel.rosero@epn.edu.ec>
2023-06-22 10:51:46 -0500

    Intent Explicito

---
+++ 01-Android/app/src/main/java/com/example/movilessoftware2023a/CIntentsExplicitoParametros.kt
@@ -0,0 +1,32 @@
+package com.example.movilessoftware2023a
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class CIntentsExplicitoParametros : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cintents_explicito_parametros)
+        val nombre = intent.getStringExtra("nombre")
+        val apellido = intent.getStringExtra("apellido")
+        val edad = intent.getIntExtra("edad", 0)
+        val boton = findViewById<Button>(R.id.btn_devolver_respuesta)
+        boton.setOnClickListener{ devolverRespuesta() }
+    }
+
+    fun devolverRespuesta(){
+        val intentDevolverParametros = Intent()
+        intentDevolverParametros
+            .putExtra("nombreModificado", "Vicente")
+        intentDevolverParametros
+            .putExtra("edadModificado", 33)
+        setResult(
+            RESULT_OK,
+            intentDevolverParametros
+        )
+        finish()
+    }
+
+}
--- 01-Android/app/src/main/java/com/example/movilessoftware2023a/MainActivity.kt
+++ 01-Android/app/src/main/java/com/example/movilessoftware2023a/MainActivity.kt
@@ -1,11 +1,52 @@
 package com.example.movilessoftware2023a
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
+import androidx.activity.result.contract.ActivityResultContracts
 
 class MainActivity : AppCompatActivity() {
+
+    val callbackContenidoIntentExplicito =
+        registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult()
+        ){
+            result ->
+            if(result.resultCode == Activity.RESULT_OK){
+                if(result.data != null){
+                    val data = result.data
+                    "${data?.getStringExtra("nombreModificado")}"
+                }
+            }
+        }
+
+    fun irActividad(
+        clase: Class<*>
+    ){
+        val intent = Intent(this, clase)
+        //NO RECIBIMOS RESPUESTA
+        startActivity(intent)
+        //this.startActivity()
+    }
+
+    fun abrirActividadConParametros(
+        clase: Class<*>
+    ){
+        val intentExplicito = Intent(this, clase)
+        //Enviar parametros
+        //(aceptamos primitivas)
+        intentExplicito.putExtra("nombre", "Adrian")
+        intentExplicito.putExtra("apellido", "Eguez")
+        intentExplicito.putExtra("edad", "30")
+        //enviamos el intent con RESPUESTA
+        //RECIBIMOS RESPUESTA
+        callbackContenidoIntentExplicito
+            .launch(intentExplicito)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
