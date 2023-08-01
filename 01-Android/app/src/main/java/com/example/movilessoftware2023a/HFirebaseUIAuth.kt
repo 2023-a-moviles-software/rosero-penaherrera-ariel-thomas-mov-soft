a518753aac9fa4f5a1c6d3fdc4ce44b7007ebb71
Ariel Rosero <ariel.rosero@epn.edu.ec>
2023-08-01 08:42:56 -0500

    Login y logout de Firebase

--- 01-Android/app/src/main/java/com/example/movilessoftware2023a/HFirebaseUIAuth.kt
+++ 01-Android/app/src/main/java/com/example/movilessoftware2023a/HFirebaseUIAuth.kt
@@ -4,9 +4,11 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import com.firebase.ui.auth.AuthUI
 import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract
 import com.firebase.ui.auth.IdpResponse
 import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult
+import com.google.firebase.auth.FirebaseAuth
 
 class HFirebaseUIAuth : AppCompatActivity() {
 
@@ -39,5 +41,30 @@ class HFirebaseUIAuth : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_hfirebase_uiauth)
+        val btnLogin = findViewById<Button>(R.id.btn_login_firebase)
+        btnLogin.setOnClickListener {
+            val providers = arrayListOf(
+                //arreglo de providers para logearse
+                //EJ: Correo, Facebook, Twitter, Google,
+                AuthUI.IdpConfig.EmailBuilder().build()
+            )
+            //Construimos el intent de login
+            val signInIntent = AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setAvailableProviders(providers)
+                .build()
+            //Respuesta del Intent de Login
+            respuestaLoginAuthUi.launch(signInIntent)
+        }
+        val btnLogout = findViewById<Button>(R.id.btn_logout_firebase)
+        btnLogout.setOnClickListener { seDeslogeo() }
+    }
+
+    fun seDeslogeo(){
+        val btnLogin = findViewById<Button>(R.id.btn_login_firebase)
+        val btnLogout = findViewById<Button>(R.id.btn_logout_firebase)
+        btnLogout.visibility = View.INVISIBLE
+        btnLogin.visibility = View.VISIBLE
+        FirebaseAuth.getInstance().signOut()
     }
 }
