6729440d6a4846fdaf543775b78bb7fba22fec20
Ariel Rosero <ariel.rosero@epn.edu.ec>
2023-08-10 12:39:43 -0500

    Aun hay errores en el deber

---
+++ Deber03-SQLite/app/src/main/java/com/example/sqlitedeber/FacultadesMenu.kt
@@ -0,0 +1,11 @@
+package com.example.sqlitedeber
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FacultadesMenu : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_facultades_menu)
+    }
+}
